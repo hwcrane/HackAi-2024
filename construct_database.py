4b8be864c192b9b42dc752646327462f8236ab89
Harry Crane <37228043+hwcrane@users.noreply.github.com>
2024-02-10 17:02:09 +0000

    done more

--- construct_database.py
+++ construct_database.py
@@ -1,15 +1,31 @@
-from langchain_community.document_loaders import TextLoader
-from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import (
+    PyPDFDirectoryLoader,
+)
+from langchain_experimental import SemanticChunker 
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
-# loader = TextLoader("./documents/CFR/CFR-2023-title14-vol1.htm")
-#
-# documents = loader.load()
-# text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-# texts = text_splitter.split_documents(documents)
-embeddings = HuggingFaceEmbeddings()
-# db = FAISS.from_documents(texts, embeddings)
+### LOAD Documents
 
-# FAISS.save_local(db, folder_path='faiss_index')
-db = FAISS.load_local("faiss_index", embeddings)
+loader = PyPDFDirectoryLoader('documents/')
+documents = loader.load()
+
+### Embeddings
+
+embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+### Split Documents
+
+text_splitter = SemanticChunker()
+texts = text_splitter.split_documents(documents)
+
+### Create Vector Database
+
+db = FAISS.from_documents(texts, embeddings)
+db.save_local('faiss_index')
+
+### Testing
+
+retriever = db.as_retriever()
+print(retriever.get_relevant_documents("What type of microphone must be installed to meet the recording requirements of paragraph (a)(2) of 14 CFR 23.1457"))
+
