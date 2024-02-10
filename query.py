ea9ba1f1e9679836868e8e6bb7ec102f1f692133
benjaminrall <benr2508@aol.com>
2024-02-10 17:16:12 +0000

    Update query.py

--- query.py
+++ query.py
@@ -3,17 +3,17 @@ from openai import OpenAI
 
 # Point to the local server
 CLIENT = OpenAI(base_url="http://localhost:1234/v1", api_key="not-needed")
+SUFFIX = ""
 
-def answer_question(prompt: str) -> str:
+def answer_question(question: str, relevant_docs) -> str:
+    
+
     completion = CLIENT.chat.completions.create(
-      model="local-model",
+      model="local-model", # this field is currently unused
       messages=[
         {"role": "system", "content": "Answer only with the option letter (A, B, C)."},
-        {"role": "user", "content": prompt + "\nAnswer only with the option letter (A, B, C)."}
+        {"role": "user", "content": PREFIX + "\n" + question + "\n" + SUFFIX}
       ],
-      temperature=0.1,
+      temperature=0.2,
     )
-    response = completion.choices[0].message.content
-    print("Response: " + response)
-    print("")
-    return "A"
+    return completion.choices[0].message
